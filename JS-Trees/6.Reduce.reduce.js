032685e840aeb4b09ff7eccd294b5a446c47d959
Konstantin Bochinin <Konstantin6487@rambler.ru>
2018-05-16 14:05:19 +0400

    Update 6.Reduce.reduce.js

--- JS-Trees/6.Reduce.reduce.js
+++ JS-Trees/6.Reduce.reduce.js
@@ -1,1 +1,41 @@
+//Реализуйте и экспортируйте по умолчанию функцию-редьюсер обрабатывающую файловые деревья.
 
+const tree = {
+    children: [
+      {
+        children: [
+          {
+            children: [],
+            meta: {},
+            name: 'NgiNx',
+            type: 'directory',
+          },
+          {
+            children: [{ meta: {}, name: 'CONFIG.json', type: 'file' }],
+            meta: {},
+            name: 'CONSUL',
+            type: 'directory',
+          },
+        ],
+        meta: {},
+        name: 'eTc',
+        type: 'directory',
+      },
+      { meta: {}, name: 'HOSTS', type: 'file' },
+    ],
+    meta: {},
+    name: '/',
+    type: 'directory',
+  };
+
+
+const reduce = (fn, tree, acc) => {
+  const { children, meta, name, type } = tree;
+  const newAcc = fn(acc, tree);
+  if (type === 'file') {
+    return newAcc;  
+  }
+  return children.reduce((iAcc, n) => reduce(fn, n, iAcc), newAcc);
+};
+
+reduce((acc, n) => acc + 1, tree, 0); //6
