c61829a7a42fa757a28c6d9571ca208e67e83eac
Konstantin Bochinin <Konstantin6487@rambler.ru>
2018-05-16 11:38:54 +0400

    Update 3.Traversal.downcaseFileNames.js

--- JS-Trees/3.Traversal.downcaseFileNames.js
+++ JS-Trees/3.Traversal.downcaseFileNames.js
@@ -1,17 +1,47 @@
 /*
 Реализуйте и экспортируйте функцию по умолчанию, которая принимает на вход директорию, приводит имена всех файлов в этой директории к нижнему регистру и возвращает ее наружу.
 
-const tree = mkdir('/', [
-  mkdir('eTc', [
-    mkdir('NgiNx'),
-    mkdir('CONSUL', [
-      mkfile('config.json'),
-    ]),
-  ]),
-  mkfile('hOsts'),
-]);
-const updatedTree = downcaseFileNames(tree);
+const tree = {
+    children: [
+      {
+        children: [
+          {
+            children: [],
+            meta: {},
+            name: 'NgiNx',
+            type: 'directory',
+          },
+          {
+            children: [{ meta: {}, name: 'CONFIG.json', type: 'file' }],
+            meta: {},
+            name: 'CONSUL',
+            type: 'directory',
+          },
+        ],
+        meta: {},
+        name: 'eTc',
+        type: 'directory',
+      },
+      { meta: {}, name: 'HOSTS', type: 'file' },
+    ],
+    meta: {},
+    name: '/',
+    type: 'directory',
+  };
 */
+
+/* if type === 'file'
+
+const dfs = tree => {
+  const { children, meta, name, type } = tree;
+  if (type === "file") {
+    return { children, meta, name: name.toLowerCase(), type }
+  }
+  return { children: children.map(dfs), meta, name, type }
+};
+*/
+
+//if type === 'directory'
 
 const dfs = tree => {
   const { children, meta, name, type } = tree;
