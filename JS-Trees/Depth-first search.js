bd1180ccc22ab7ef43eefbc78ae2a1792472cd27
Konstantin Bochinin <Konstantin6487@rambler.ru>
2018-05-16 11:17:11 +0400

    Update Depth-first search.js

--- JS-Trees/Depth-first search.js
+++ JS-Trees/Depth-first search.js
@@ -1,1 +1,17 @@
 
+//Elements without children
+
+const dfs = tree => {
+  const [element, children] = tree;
+  return !children ? undefined : children.map(dfs);
+};
+
+dfs(['A', 
+      [
+        ['B', [['E'], ['F']]],
+        ['C'],
+        ['D', [['G'], ['J']]],
+      ]
+]);
+
+//[[ undefined, undefined ], undefined, [ undefined, undefined ]]
