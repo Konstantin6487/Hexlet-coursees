bde8a720358f49871c482b1f5d26ab7cbb65c017
Konstantin Bochinin <Konstantin6487@rambler.ru>
2018-05-16 11:48:09 +0400

    Update 3.Traversal.Map.js

--- JS-Trees/3.Traversal.Map.js
+++ JS-Trees/3.Traversal.Map.js
@@ -1,1 +1,43 @@
+/*
 
+Реализуйте и экспортируйте по умолчанию функцию, которая принимает на вход функцию-обработчик и дерево, а возвращает отображенное дерево.
+
+const tree = {
+    children: [
+      {
+        children: [
+          {
+            children: [],
+            meta: {},
+            name: 'NgiNx',
+            type: 'directory',
+          },
+          {
+            children: [{ meta: {}, name: 'CONFIG.json', type: 'file' }],
+            meta: {},
+            name: 'CONSUL',
+            type: 'directory',
+          },
+        ],
+        meta: {},
+        name: 'eTc',
+        type: 'directory',
+      },
+      { meta: {}, name: 'HOSTS', type: 'file' },
+    ],
+    meta: {},
+    name: '/',
+    type: 'directory',
+  };
+*/
+
+const map = (fn, tree) => {
+  const { children, meta, name, type } = tree;
+  const newName = fn(tree);
+  if (newName.type === "directory") {
+    return { ...newName, children: children.map(c => map(fn, c))};
+  }
+  return newName;
+};
+
+map(n => ({ ...n, name: n.name.toUpperCase() }), tree);
